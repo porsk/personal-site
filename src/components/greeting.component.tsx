1a119c9d18c7202e08832e732a06a8ef853bc84e
porsk <p.krisztian@outlook.com>
2022-12-28 00:41:32 +0200

    #31 - Update greeting.

--- src/components/greeting.component.tsx
+++ src/components/greeting.component.tsx
@@ -9,6 +9,7 @@ import {
 	Hidden,
 } from '@mui/material';
 import { FC } from 'react';
+import useScrollToSection from '../hooks/useScrollToSection';
 
 const containerStyle = (theme: any) =>
 	css({
@@ -53,56 +54,61 @@ const avatarStyle = (theme: any) =>
 		},
 	});
 
-const Greeting: FC = () => (
-	<Container css={containerStyle}>
-		<Grid container rowSpacing={8}>
-			<Grid item xs={12} md="auto">
-				<Typography variant="h6" color="primary" css={textStyle}>
-					Hi, my name is
-				</Typography>
-				<Typography variant="h2" sx={{ fontWeight: 400 }}>
-					Krisztián Patakfalvi.
-				</Typography>
-				<Typography
-					variant="h3"
-					color="text.secondary"
-					css={textStyle}
-					sx={{ fontWeight: 300 }}
-				>
-					I bring ideas to life with code.
-				</Typography>
+const Greeting: FC = () => {
+	const { scrollToSection } = useScrollToSection();
 
-				<Typography
-					variant="h6"
-					color="text.secondary"
-					css={[textStyle, headlineStyle]}
-					maxWidth={500}
-				>
-					I&apos;m a Full Stack Developer specializing in JavaScript
-					across the whole stack, spiced with a great deal of
-					enthusiasm towards DevOps and Kubernetes.
-				</Typography>
-
-				<Hidden mdDown>
-					<Button
-						variant="outlined"
-						size="large"
-						sx={{ marginTop: 3 }}
+	return (
+		<Container css={containerStyle}>
+			<Grid container rowSpacing={8}>
+				<Grid item xs={12} md="auto">
+					<Typography variant="h6" color="primary" css={textStyle}>
+						Hi, my name is
+					</Typography>
+					<Typography variant="h2" sx={{ fontWeight: 400 }}>
+						Krisztián Patakfalvi.
+					</Typography>
+					<Typography
+						variant="h3"
+						color="text.secondary"
+						css={textStyle}
+						sx={{ fontWeight: 300 }}
 					>
-						Get in touch
-					</Button>
-				</Hidden>
-			</Grid>
+						I bring ideas to life with code.
+					</Typography>
 
-			<Grid item xs={12} md css={avatarContainerStyle}>
-				<Avatar
-					alt="Remy Sharp"
-					src={`${window.location.origin}/android-chrome-512x512.png`}
-					css={avatarStyle}
-				/>
+					<Typography
+						variant="h6"
+						color="text.secondary"
+						css={[textStyle, headlineStyle]}
+						maxWidth={500}
+					>
+						I&apos;m a Full Stack JavaScript Developer, eager to
+						solve creative problems using cutting edge technologies,
+						and move big ideas from design to implementation.
+					</Typography>
+
+					<Hidden mdDown>
+						<Button
+							variant="outlined"
+							size="large"
+							sx={{ marginTop: 3 }}
+							onClick={() => scrollToSection('Contact')}
+						>
+							Get in touch
+						</Button>
+					</Hidden>
+				</Grid>
+
+				<Grid item xs={12} md css={avatarContainerStyle}>
+					<Avatar
+						alt="Krisztián Patakfalvi"
+						src={`${window.location.origin}/android-chrome-512x512.png`}
+						css={avatarStyle}
+					/>
+				</Grid>
 			</Grid>
-		</Grid>
-	</Container>
-);
+		</Container>
+	);
+};
 
 export default Greeting;
